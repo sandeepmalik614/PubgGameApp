7cb724f6ce6e2128f7961b98732312081268d991
Iknoortech <android@hkdigitalonline.com>
2020-02-11 15:49:18 +0530

    second commit

--- app/src/main/java/com/gameapp/activity/ForgotPasswordActivity.java
+++ app/src/main/java/com/gameapp/activity/ForgotPasswordActivity.java
@@ -12,5 +12,6 @@ public class ForgotPasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        
     }
 }
